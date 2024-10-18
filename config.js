93fc0f18c1b32fd903d5fcb28e8809bfefcd12bb
0xA2d3r <clintsniff@gmail.com>
2024-10-18 23:32:59 +1100

    removed secret and now adding open ai api call

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+    OPENAI_API_KEY: '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'
+};
